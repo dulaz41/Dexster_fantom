879699fd380331bb34540fb791b2ce01fb136f87
dulaz41 <dulaz41@yahoo.com>
2023-03-03 00:02:22 +0100

    Smart contract upload

---
+++ smart-contract/scripts/deploy.js
@@ -0,0 +1,33 @@
+const hre = require("hardhat");
+
+async function main() {
+  const FaucetContract = await hre.ethers.getContractFactory("Faucet");
+  const faucet = await FaucetContract.deploy();
+  await faucet.deployed();
+
+  console.log("Faucet Contract deployed to:", faucet.address);
+
+  const DexContract = await hre.ethers.getContractFactory("Dex");
+  const dex = await DexContract.deploy();
+  await dex.deployed();
+
+  console.log("Dex Contract deployed to:", dex.address);
+
+  const token1 = ;
+  const token2 = ;
+
+  const LiquidityPoolContract = await hre.ethers.getContractFactory(
+    "LiquidityPool"
+  );
+  const liquidityPool = await LiquidityPoolContract.deploy(token1,token2);
+  await liquidityPool.deployed();
+
+  console.log("liquidityPool Contract deployed to:", liquidityPool.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
