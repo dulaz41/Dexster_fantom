3242e70a250ea5e630450c4934ef0002c49ae038
dulaz41 <dulaz41@yahoo.com>
2023-03-04 23:30:08 +0100

    Updated deploy.js

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -7,12 +7,6 @@ async function main() {
 
   console.log("Faucet Contract deployed to:", faucet.address);
 
-  const DexContract = await hre.ethers.getContractFactory("Dex");
-  const dex = await DexContract.deploy();
-  await dex.deployed();
-
-  console.log("Dex Contract deployed to:", dex.address);
-
   const DAI_Token = "0x82d984B5adfD7796D7fDFa4403726478dD341DE7";
   const FTM_Token = "0x3c355d9327819F1cbD7A4650C51b5449ac285266";
 
@@ -26,6 +20,12 @@ async function main() {
   await liquidityPool.deployed();
 
   console.log("liquidityPool Contract deployed to:", liquidityPool.address);
+
+  const DexContract = await hre.ethers.getContractFactory("Dex");
+  const dex = await DexContract.deploy();
+  await dex.deployed();
+
+  console.log("Dex Contract deployed to:", dex.address);
 }
 
 main()
