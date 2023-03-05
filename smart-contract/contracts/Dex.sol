21fadd5dc2449d2e2de8dcce5ef6a3685c08322b
dulaz41 <dulaz41@yahoo.com>
2023-03-06 00:08:25 +0100

    Updated dex.sol

--- smart-contract/contracts/Dex.sol
+++ smart-contract/contracts/Dex.sol
@@ -2,6 +2,9 @@
 
 pragma solidity ^0.8.0;
 
+import "./interfaces/IUniswapV2Router.sol";
+import "./interfaces/IUniswapV2Pair.sol";
+import "./interfaces/IUniswapV2Factory.sol";
 import "./LiquidityPool.sol";
 
 contract Dex {
@@ -9,6 +12,11 @@ contract Dex {
     address public immutable owner;
     // Array of liquidity pool addresses
     address[] public liquidityPools;
+
+    address private constant ROUTER =
+        0xF491e7B69E4244ad4002BC14e878a34207E38c29;
+
+    address private constant WFTM = 0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83;
 
     // Mapping to get address of liquidity pool with token addresses
     mapping(address => mapping(address => address)) public getLiquidityPool;
@@ -24,10 +32,10 @@ contract Dex {
         owner = msg.sender;
     }
 
-    function createLiquidityPool(address _tokenA, address _tokenB)
-        external
-        returns (address _liquidityPool)
-    {
+    function createLiquidityPool(
+        address _tokenA,
+        address _tokenB
+    ) external returns (address _liquidityPool) {
         (address _token1, address _token2) = _tokenA < _tokenB
             ? (_tokenA, _tokenB)
             : (_tokenB, _tokenA);
@@ -47,5 +55,60 @@ contract Dex {
         liquidityPools.push(_liquidityPool);
 
         emit LiquidityPoolCreted(_token1, _token2, _liquidityPool);
+    }
+
+    function swap(
+        address _tokenIn,
+        address _tokenOut,
+        uint256 _amountIn,
+        uint256 _amountOutMin,
+        address _to
+    ) external {
+        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
+
+        IERC20(_tokenIn).approve(ROUTER, _amountIn);
+
+        address[] memory path;
+        if (_tokenIn == WFTM || _tokenOut == WFTM) {
+            path = new address[](2);
+            path[0] = _tokenIn;
+            path[1] = _tokenOut;
+        } else {
+            path = new address[](3);
+            path[0] = _tokenIn;
+            path[1] = WFTM;
+            path[2] = _tokenOut;
+        }
+        IUniswapV2Router(ROUTER).swapExactTokensForTokens(
+            _amountIn,
+            _amountOutMin,
+            path,
+            _to,
+            block.timestamp
+        );
+    }
+
+    function getAmountOutMin(
+        address _tokenIn,
+        address _tokenOut,
+        uint256 _amountIn
+    ) external view returns (uint256) {
+        address[] memory path;
+        if (_tokenIn == WFTM || _tokenOut == WFTM) {
+            path = new address[](2);
+            path[0] = _tokenIn;
+            path[1] = _tokenOut;
+        } else {
+            path = new address[](3);
+            path[0] = _tokenIn;
+            path[1] = WFTM;
+            path[2] = _tokenOut;
+        }
+
+        uint256[] memory amountOutMins = IUniswapV2Router(ROUTER).getAmountsOut(
+            _amountIn,
+            path
+        );
+        return amountOutMins[path.length - 1];
     }
 }
--- smart-contract/contracts/FaucetContract.sol
+++ smart-contract/contracts/FaucetContract.sol
@@ -1,101 +1,27 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
-contract Faucet {
-    uint256 transactionCount;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    event Transfer(
-        address from,
-        address receiver,
-        uint amount,
-        string message,
-        uint256 timestamp
-    );
+contract faucet is ERC20 {
+    uint public amountAllowed = 1000000000000000000;
 
-    struct TransferStruct {
-        address sender;
-        address receiver;
-        uint amount;
-        string message;
-        uint256 timestamp;
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 50000000 * (10 ** 18));
     }
 
-    TransferStruct[] transactions;
+    mapping(address => uint) public lockTime;
 
-    uint public numOfFunders;
-
-    mapping(address => bool) private funders;
-    mapping(uint => address) private lutFunders;
-
-    modifier limitWithdraw(uint withdrawAmount) {
+    function requestTokens(address requestor, uint amount) external {
         require(
-            withdrawAmount <= 100000000000000000,
-            "Cannot withdraw more than 0.1 ether"
-        );
-        _;
-    }
-
-    receive() external payable {}
-
-    function addFunds() external payable {
-        address funder = msg.sender;
-
-        if (!funders[funder]) {
-            uint index = numOfFunders++;
-            funders[funder] = true;
-            lutFunders[index] = funder;
-        }
-    }
-
-    function addToBlockchain(
-        address payable receiver,
-        uint amount,
-        string memory message
-    ) public {
-        transactionCount += 1;
-        transactions.push(
-            TransferStruct(
-                msg.sender,
-                receiver,
-                amount,
-                message,
-                block.timestamp
-            )
+            block.timestamp > lockTime[msg.sender],
+            "lock time has not expired. Please try again later"
         );
 
-        emit Transfer(msg.sender, receiver, amount, message, block.timestamp);
-    }
+        _mint(requestor, amount);
 
-    function withdraw(
-        uint withdrawAmount
-    ) external limitWithdraw(withdrawAmount) {
-        payable(msg.sender).transfer(withdrawAmount);
-    }
-
-    function getAllTransactions()
-        public
-        view
-        returns (TransferStruct[] memory)
-    {
-        return transactions;
-    }
-
-    function getTransactionCount() public view returns (uint256) {
-        return transactionCount;
-    }
-
-    function getAllFunders() external view returns (address[] memory) {
-        address[] memory _funders = new address[](numOfFunders);
-
-        for (uint i = 0; i < numOfFunders; i++) {
-            _funders[i] = lutFunders[i];
-        }
-
-        return _funders;
-    }
-
-    function getFunderAtIndex(uint8 index) external view returns (address) {
-        return lutFunders[index];
+        //updates locktime 1 day from now
+        lockTime[msg.sender] = block.timestamp + 1 days;
     }
 }
